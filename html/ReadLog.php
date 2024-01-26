fc5c15a2c82febc11d139f63fe5337356b6831dd
Karsten Siegmund <gitbengelsystem.us.iridos@recursor.net>
2024-01-26 08:21:59 +0100

    bugfix double-require

--- html/ReadLog.php
+++ html/ReadLog.php
@@ -2,7 +2,7 @@
 // Login und Admin Status testen. Wenn kein Admin-Status, Weiterleiten auf index.php und beenden
 require_once 'konfiguration.php';
 SESSION_START();
-require 'SQL.php';
+require_once 'SQL.php';
 $db_link = ConnectDB();
 require '_login.php';
 
@@ -28,7 +28,6 @@ require '_login.php';
   //check for admin status
   $HelferID = $_SESSION["HelferID"];
   $AdminID = $_SESSION["AdminID"];
-  require 'SQL.php';
   $db_link = mysqli_connect(MYSQL_HOST, MYSQL_BENUTZER, MYSQL_KENNWORT, MYSQL_DATENBANK);
   DatenbankAufDeutsch($db_link);
 
